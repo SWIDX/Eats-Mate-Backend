66128e8f65624aafde10948c6f0770173485054a
onyoo <dhsdb02@gmail.com>
2022-07-21 20:17:22 +0900

    feat(core): Add restaurant name search repository

--- map-service/src/main/java/com/swidx/mapservice/Repository/InformationRepository.java
+++ map-service/src/main/java/com/swidx/mapservice/Repository/InformationRepository.java
@@ -4,8 +4,11 @@ import com.swidx.mapservice.entity.InformationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface InformationRepository extends JpaRepository<InformationEntity,Integer>{
 
-
+    List<InformationEntity> findByNameLike(String name);
 }
