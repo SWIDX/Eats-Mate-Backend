cd48bf2b8831a379750012a93a02745241045700
xeangit <mily072@naver.com>
2022-07-22 20:35:21 +0900

    feat: Add review post crud

---
+++ review-service/src/main/java/com/swidx/reviewservice/controller/ReviewApiController.java
@@ -0,0 +1,56 @@
+package com.swidx.reviewservice.controller;
+
+
+import com.swidx.reviewservice.controller.dto.ReviewResponseDto;
+import com.swidx.reviewservice.controller.dto.ReviewSaveRequestDto;
+import com.swidx.reviewservice.controller.dto.ReviewUpdateRequestDto;
+import com.swidx.reviewservice.domain.Review;
+import com.swidx.reviewservice.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.print.Pageable;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/review")
+public class ReviewApiController {
+    private final ReviewService reviewService;
+
+    //create
+    @PostMapping()
+    public ResponseEntity<ReviewResponseDto> createReview(@RequestBody ReviewSaveRequestDto requestDto){
+        return new ResponseEntity<>(reviewService.save(requestDto), HttpStatus.OK);
+    }
+
+    //read
+    @GetMapping(value = "/{idx}")
+    public ResponseEntity<ReviewResponseDto> readReview(@PathVariable Long idx){
+        return new ResponseEntity<>(reviewService.readOne(idx),HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public List<Review> readAllReview(){
+        List<Review> listReview = reviewService.readAll();
+        return listReview;
+    }
+
+
+    //update
+    @PutMapping("/{idx}")
+    public ResponseEntity updateReview(@PathVariable Long idx, @RequestBody ReviewUpdateRequestDto requestDto){
+        return new ResponseEntity<>(reviewService.update(idx, requestDto),HttpStatus.OK);
+    }
+
+    //delete
+    @DeleteMapping("/{idx}")
+    public ResponseEntity deleteReview(@PathVariable Long idx){
+        reviewService.delete(idx);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
