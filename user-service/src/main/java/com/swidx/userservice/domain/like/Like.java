6edb720ec8277d5972d0db791f1e229263c3d2ff
Queue-ri <qriositylog@gmail.com>
2022-08-21 15:31:26 +0900

    feat: Update Like entity

--- user-service/src/main/java/com/swidx/userservice/domain/like/Like.java
+++ user-service/src/main/java/com/swidx/userservice/domain/like/Like.java
@@ -1,23 +1,28 @@
 package com.swidx.userservice.domain.like;
 
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
-
 import javax.persistence.*;
 import java.util.UUID;
 
+@NoArgsConstructor
+@Table(name = "user_like")
 @Entity
 public class Like {
     @Id
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(columnDefinition = "BINARY(16)")
-    private UUID like_id;
+    private UUID likeId;
 
     @Column(nullable = false)
     private String email;
 
     @Column(nullable = false)
-    private Long place_id;
+    private Long placeId;
 
-    // WIP..
+    public Like(String email, Long placeId) {
+        this.email = email;
+        this.placeId = placeId;
+    }
 }
