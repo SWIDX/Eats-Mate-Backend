e94d0acd03c0572e4ade8ea7ab3ed77839f32ef0
Queue-ri <qriositylog@gmail.com>
2022-08-22 20:17:33 +0900

    feat: Add queries with `WHERE` to LikeRepository

--- user-service/src/main/java/com/swidx/userservice/domain/like/LikeRepository.java
+++ user-service/src/main/java/com/swidx/userservice/domain/like/LikeRepository.java
@@ -2,7 +2,10 @@ package com.swidx.userservice.domain.like;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface LikeRepository extends JpaRepository<Like, UUID> {
+    Optional<Like> findByEmailAndPlaceId(String email, Long placeId); // WHERE .. AND ..
+    Optional<Like> deleteByEmailAndPlaceId(String email, Long placeId);
 }
