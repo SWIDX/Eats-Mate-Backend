4d5bc01be8b7e2d769c2882df43ad04e6a29a5ea
onyoo <dhsdb02@gmail.com>
2022-08-25 16:01:23 +0900

    test: Change folder information controller test code

--- map-service/src/test/java/com/swidx/mapservice/controller/InformationControllerTest.java
+++ map-service/src/test/java/com/swidx/mapservice/controller/InformationControllerTest.java
@@ -1,4 +1,4 @@
-package com.swidx.mapservice.repository;
+package com.swidx.mapservice.controller;
 
 import com.swidx.mapservice.controller.InformationController;
 import org.junit.jupiter.api.DisplayName;
@@ -8,9 +8,11 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 @AutoConfigureMockMvc
+@WebAppConfiguration
 @WebMvcTest(InformationController.class)
 public class InformationControllerTest {
 
