973cae9cdf4ed89b37b0da20e0d88b5d40330f59
onyoo <dhsdb02@gmail.com>
2022-08-25 16:06:05 +0900

    feat(core): Modify restaurant name search api return type

--- map-service/src/main/java/com/swidx/mapservice/controller/InformationController.java
+++ map-service/src/main/java/com/swidx/mapservice/controller/InformationController.java
@@ -3,22 +3,20 @@ package com.swidx.mapservice.controller;
 import com.swidx.mapservice.Repository.InformationRepository;
 import com.swidx.mapservice.entity.InformationEntity;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.ws.rs.Path;
-import java.awt.*;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/map-service")
+
 public class InformationController {
     private final InformationRepository informationRepository;
 
     @GetMapping("information/get_all")
-    public List<InformationEntity> findAllMember(){
+    public List<InformationEntity> findAllInformation(){
         return informationRepository.findAll();
     }
 
@@ -28,12 +26,12 @@ public class InformationController {
     }
 
     @GetMapping("information/findByName/{name}")
-    public InformationEntity findByNameLike(@PathVariable String name){
+    public List<InformationEntity> findByNameLike(@PathVariable String name){
          List<InformationEntity> result = informationRepository.findByNameLike("%"+name+"%");
          if(result.isEmpty()){
              return null;
          }else {
-             return result.get(0);
+             return result;
          }
     }
 
