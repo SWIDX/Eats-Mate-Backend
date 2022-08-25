96f223ad440592bfe8dbdbd8d891e0f379a8d0c0
onyoo <dhsdb02@gmail.com>
2022-08-25 16:04:09 +0900

    chore: Delete unused modules

--- map-service/src/main/java/com/swidx/mapservice/Repository/InformationRepository.java
+++ map-service/src/main/java/com/swidx/mapservice/Repository/InformationRepository.java
@@ -4,10 +4,8 @@ import com.swidx.mapservice.entity.InformationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.math.BigDecimal;
 import java.util.List;
-import java.util.Optional;
+
 
 @Repository
 public interface InformationRepository extends JpaRepository<InformationEntity,Integer>{
