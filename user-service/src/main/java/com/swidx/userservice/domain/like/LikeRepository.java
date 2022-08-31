95b972435662bff9c80746d748ed2e8b34d87ad4
Queue-ri <qriositylog@gmail.com>
2022-09-01 02:57:08 +0900

    feat: Modify LikeRepository

--- user-service/src/main/java/com/swidx/userservice/domain/like/LikeRepository.java
+++ user-service/src/main/java/com/swidx/userservice/domain/like/LikeRepository.java
@@ -1,11 +1,12 @@
 package com.swidx.userservice.domain.like;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface LikeRepository extends JpaRepository<Like, UUID> {
     Optional<Like> findByEmailAndPlaceId(String email, Long placeId); // WHERE .. AND ..
-    Optional<Like> deleteByEmailAndPlaceId(String email, Long placeId);
+    void deleteByEmailAndPlaceId(@Param("email") String email, @Param("placeId") Long placeId);
 }
