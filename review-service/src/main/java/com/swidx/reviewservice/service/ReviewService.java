16927e0ec297dc644ed74a5668b16deef5953a8d
Queue-ri <qriositylog@gmail.com>
2022-09-01 02:39:07 +0900

    feat: Modify review save REST API

--- review-service/src/main/java/com/swidx/reviewservice/service/ReviewService.java
+++ review-service/src/main/java/com/swidx/reviewservice/service/ReviewService.java
@@ -4,8 +4,13 @@ import com.swidx.reviewservice.controller.dto.ReviewResponseDto;
 import com.swidx.reviewservice.controller.dto.ReviewSaveRequestDto;
 import com.swidx.reviewservice.controller.dto.ReviewUpdateRequestDto;
 import com.swidx.reviewservice.domain.Review;
+import com.swidx.reviewservice.feign.client.JwtValidationClient;
 import com.swidx.reviewservice.repository.ReviewRepository;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,16 +19,29 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
-
+    @Autowired
+    private final JwtValidationClient jwtClient;
     private final ReviewRepository reviewRepository;
 
     //crud
 
     //create
-    public ReviewResponseDto save(ReviewSaveRequestDto requestDto){
-        Review review = ReviewSaveRequestDto.toEntity();
+    public ResponseEntity save(String accessToken,
+                               ReviewSaveRequestDto requestDto){
+        // feign client로 jwt 검증
+        String email = null;
+        try {
+            email = jwtClient.getUserEmail(accessToken);
+        } catch (FeignException e) {
+            System.out.println(accessToken);
+            System.out.println("*** ReviewService: JWT client failed ***");
+            System.out.println(e);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST); // 나중에 e 상태 보고 코드 쪼개서 오류 처리해야 함
+        }
+
+        Review review = ReviewSaveRequestDto.toEntity(email, requestDto);
         reviewRepository.save(review);
-        return new ReviewResponseDto(review);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     //read
@@ -38,10 +56,14 @@ public class ReviewService {
     }
 
     //update
-    public Long update(Long idx, ReviewUpdateRequestDto requestDto){
-        Review review = reviewRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("일치하는 순서가 없습니다"));
-        review.update(requestDto.getContent(), requestDto.getCategory(), requestDto.getImage(), requestDto.getRate());
-        return id;
+    public void update(Long id, ReviewUpdateRequestDto requestDto){
+        // feign으로 jwt 검증
+        String email = "";
+
+        Review review = reviewRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("*** ReviewService: 해당 리뷰 id가 존재하지 않습니다. ***")
+        );
+        review.update(email, requestDto);
     }
 
     //delete
