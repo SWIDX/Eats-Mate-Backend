4b7d8153a0ea55356dd8bc8ca93b5d7732eba10e
Queue-ri <qriositylog@gmail.com>
2022-09-01 02:36:20 +0900

    feat: Modify ReviewUpdateRequestDto

--- review-service/src/main/java/com/swidx/reviewservice/controller/dto/ReviewUpdateRequestDto.java
+++ review-service/src/main/java/com/swidx/reviewservice/controller/dto/ReviewUpdateRequestDto.java
@@ -1,32 +1,18 @@
 package com.swidx.reviewservice.controller.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class ReviewUpdateRequestDto {
 
-    private String placeName;
-    private String email;
+    private Long id;
     private String content;
-    private String category;
     private String image;
     private Integer rate;
-    private Integer recommend;
-    
-
-
-    @Builder
-    public ReviewUpdateRequestDto(String placeName, String email, String content, String category, String image, Integer rate, Integer recommend){
-        this.placeName = placeName;
-        this.email = email;
-        this.content = content;
-        this.category = category;
-        this.image = image;
-        this.rate = rate;
-        this.recommend = recommend;
-    }
 }
