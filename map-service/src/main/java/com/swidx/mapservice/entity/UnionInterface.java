f53c2df7ebbb299eaf1f8b616ac28bb4a1f3ab26
onyoo <dhsdb02@gmail.com>
2022-09-03 03:40:42 +0900

    feat(core): Add UnionData Interface

---
+++ map-service/src/main/java/com/swidx/mapservice/entity/UnionInterface.java
@@ -0,0 +1,11 @@
+package com.swidx.mapservice.entity;
+
+public interface UnionInterface {
+    Integer getId();
+    String getName();
+    String getGubun();
+    String getAddress();
+    String getType();
+
+
+}
