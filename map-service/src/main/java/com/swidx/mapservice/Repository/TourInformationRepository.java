c65bb02a9d9382603ddd3081200d74f1d7db7158
Queue-ri <qriositylog@gmail.com>
2022-09-04 17:18:55 +0900

    fix(core): Modify getAllData api to return lat & lng

--- map-service/src/main/java/com/swidx/mapservice/Repository/TourInformationRepository.java
+++ map-service/src/main/java/com/swidx/mapservice/Repository/TourInformationRepository.java
@@ -24,11 +24,11 @@ public interface TourInformationRepository extends JpaRepository<TourInformation
     @Query(value = "select *,'관광지' as type from tour_information where (name like %:text% or address like %:text% )",nativeQuery = true)
     List<TourInformationEntity> getSearchTourInfo(String text);
 
-    @Query(value ="select id ,name,gubun,address,'음식점' as type from map_service.information " +
+    @Query(value ="select id, name, gubun, lat, lng, address, '음식점' as type from map_service.information " +
             "where (name like %:text% or address like %:text% or gugun like %:text%) " +
-            "union select content_id as id,name,gubun,address,'관광지' as type " +
+            "union select content_id as id, name, gubun, lat, lng, address, '관광지' as type " +
             "from map_service.tour_information where (name like %:text% or address " +
-            "like %:text% ) ORDER BY name desc ",countQuery = "select id ,name,gubun,address,'음식점' as type from map_service.information where (name like %:text% or address like %:text% or gugun like %:text%) union select content_id as id,name,gubun,address,'관광지' as type from map_service.tour_information where (name like %:text% or address like %:text% ) ORDER BY name desc ",nativeQuery = true)
+            "like %:text% ) ORDER BY name asc ", countQuery = "select id, name, gubun, address, '음식점' as type from map_service.information where (name like %:text% or address like %:text% or gugun like %:text%) union select content_id as id, name, gubun, address, '관광지' as type from map_service.tour_information where (name like %:text% or address like %:text% ) ORDER BY name asc ",nativeQuery = true)
     List<UnionInterface> getSearchUnionInformation(String text);
 
     @Query(value="select * from tour_information where content_id = :contentsId",nativeQuery = true)
